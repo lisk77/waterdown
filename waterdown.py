0e65e3a8df2cc8732fc5228aab07ca8482796dae
lisk77 <44212715+lisk77@users.noreply.github.com>
2022-08-21 15:47:10 +0200

    Update waterdown.py

--- waterdown.py
+++ waterdown.py
@@ -1,39 +1,51 @@
 fileName = "test"
 template = "index"
 
+with open(f"./test/{fileName}.md", "r") as f:
+    lines = f.readlines()
+
+splitted = "".join(lines).split("\n")
+
+interpreted = []
+
+def paragraphParse(line):
+    if "{" and "}" in line:
+        _line = list(line)
+        _line.pop(-1)
+        image = _line[1:]
+        if "," in image:
+            before, after = "".join(image).split(",")
+            width, height = "".join(after).split("x")
+            if " " in list(width):
+                _width = list(width)
+                _width.pop(0)
+                width = "".join(_width)
+        interpreted.append(f"<img src='{before}' width='{width}' height='{height}'/>")
+    elif "[" and "]" in line:
+        _link = line.split("[")
+        link_ = line.split("]")
+        link = [_link[0], _link[1], link_[-1]]
+        comma = link[1].split(",")
+        if comma[1].startswith(" "):
+            t = list(comma[1])
+            t.pop(0)
+            y = "".join(t)
+        x = y.replace("]", "")
+        comma[1] = x
+        interpreted.append(f"<p>{link[0]}<a href='{comma[1]}'>{comma[0]}</a>{link[2]}</p>")
+    elif "*" in line:
+        bold = line.split("*")
+        interpreted.append(f"<p>{bold[0]}<b>{bold[1]}</b>{bold[2]}</p>")
+    elif "/" in line:
+        italic = line.split("/")
+        interpreted.append(f"<p>{italic[0]}<i>{italic[1]}</i>{italic[2]}</p>")
+    elif "~" in line:
+        strike = line.split("~")
+        interpreted.append(f"<p>{strike[0]}<s>{strike[1]}</s>{strike[2]}</p>")
+    else:
+        interpreted.append(f"<p>{line}</p>")
+
 def main():
-    with open(f"{fileName}.md", "r") as f:
-        lines = f.readlines()
-
-    splitted = "".join(lines).split("\n")
-
-    interpreted = []
-
-    def paragraphParse(line):
-        if "[" and "]" in line:
-            _link = line.split("[")
-            link_ = line.split("]")
-            link = [_link[0], _link[1], link_[-1]]
-            comma = link[1].split(",")
-            if comma[1].startswith(" "):
-                t = list(comma[1])
-                t.pop(0)
-                y = "".join(t)
-            x = y.replace("]", "")
-            comma[1] = x
-            interpreted.append(f"<p>{link[0]}<a href='{comma[1]}'>{comma[0]}</a>{link[2]}</p>")
-        elif "*" in line:
-            bold = line.split("*")
-            interpreted.append(f"<p>{bold[0]}<b>{bold[1]}</b>{bold[2]}</p>")
-        elif "/" in line:
-            italic = line.split("/")
-            interpreted.append(f"<p>{italic[0]}<i>{italic[1]}</i>{italic[2]}</p>")
-        elif "~" in line:
-            strike = line.split("~")
-            interpreted.append(f"<p>{strike[0]}<s>{strike[1]}</s>{strike[2]}</p>")
-        else:
-            interpreted.append(f"<p>{line}</p>")
-
     for i in splitted:
         j = i.split(" ")
         if j[0] == "#":
@@ -56,9 +68,9 @@ def main():
     for i,j in enumerate(interpreted):
         html.insert(index+i+1, j)
 
-    with open(f"{fileName}.html", "a") as nind:
+    with open(f"./test/{fileName}.html", "a") as nind:
         for i in html:
             nind.writelines(i + "\n")
-        
+
 if __name__ == "__main__":
     main()
